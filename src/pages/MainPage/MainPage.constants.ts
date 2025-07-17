21062a2427b72e856c8862a3c0423b16b7919f4a
dusixx <113414935+dusixx@users.noreply.github.com>
2025-07-17 18:09:32 +0300

    refactor: update MainPage

--- src/pages/MainPage/MainPage.constants.ts
+++ src/pages/MainPage/MainPage.constants.ts
@@ -1,3 +1,4 @@
+export const LS_KEY_LAST_QUERY = 'last-query-djh501';
 export const QUERY_PLACEHOLDER = 'Input name (eg. "rick") or smth like "11" to see the error...';
 export const LOADER_VISIBILITY_DURATION = 500;
 export const ERROR_ICON_PROPS = {
--- src/pages/MainPage/MainPage.utils.ts
+++ src/pages/MainPage/MainPage.utils.ts
@@ -1,6 +1,5 @@
 import { isError, isString } from '@utils/index.ts';
-
-const LS_KEY_LAST_QUERY = 'last-query-djh501';
+import { LS_KEY_LAST_QUERY } from './MainPage.constants.ts';
 
 export const getPersistedQuery = (): string => {
   return localStorage.getItem(LS_KEY_LAST_QUERY) ?? '';
