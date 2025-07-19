16749f7c59bbfee8c302598644c133c8dce91ee0
dusixx <113414935+dusixx@users.noreply.github.com>
2025-07-19 18:42:59 +0300

    feat: update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -24,3 +24,25 @@ export const areStringsEqual = (
 ): boolean => {
   return str1.localeCompare(str2, locales, ignoreCase ? { sensitivity: 'base' } : undefined) === 0;
 };
+
+export const serializeStyle = (style: object): string => {
+  return JSON.stringify(style)
+    .replace(/[^\w:,.]/gi, '')
+    .replace(/,/g, ';');
+};
+
+export const rndInt = (min: number, max: number): number => {
+  return Math.round(min + Math.random() * (max - min));
+};
+
+export const chooseOneRandomly = <T>(...values: T[]): T => {
+  return values[rndInt(0, values.length - 1)];
+};
+
+export const getErrorInstance = (error: unknown): Error | undefined => {
+  return isError(error) ? error : isString(error) ? Error(error) : undefined;
+};
+
+export const getErrorMessage = (error: unknown, defaultMessage: string = ''): string => {
+  return isString(error) ? error : isError(error) ? error.message : defaultMessage;
+};
