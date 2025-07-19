bafcfe7895dddecfc12df76e6e4e1341c7bbad20
dusixx <113414935+dusixx@users.noreply.github.com>
2025-07-19 20:01:25 +0300

    chore: update vitest setup

--- src/test-utils/vitest.setup.ts
+++ src/test-utils/vitest.setup.ts
@@ -2,6 +2,9 @@ import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach, vi } from 'vitest';
 
+vi.spyOn(console, 'error').mockImplementation(() => {});
+vi.spyOn(console, 'log').mockImplementation(() => {});
+
 afterEach(() => {
   cleanup();
   vi.clearAllMocks();
