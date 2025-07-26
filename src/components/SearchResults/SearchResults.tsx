f7ca0ed8110e815b5314c908e59c90ee80986889
dusixx <113414935+dusixx@users.noreply.github.com>
2025-07-26 09:09:19 +0300

    feat: update SearchResults

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,52 +1,46 @@
-import { ERR_SOMETHING_WRONG, IconCloseCircleOutline } from '@common/constants.ts';
+import { ERR_SOMETHING_WRONG, LOADER_VISIBILITY_DURATION } from '@common/constants.ts';
+import { getErrorInstance, getErrorMessage, isNumericInteger } from '@common/utils';
 import { CardList } from '@components/CardList/CardList.tsx';
+import { ErrorInfo } from '@components/ErrorInfo/ErrorInfo.tsx';
+import { Loader } from '@components/Loader/Loader';
 import { useCustomSearchParams } from '@hooks/useCustomSearchParams.ts';
 import { getCharactersByName } from '@services/api.ts';
 import type { CharacterInfo } from '@services/api.types.ts';
-import { getErrorInstance, getErrorMessage, isNumericInteger } from '@utils/index.ts';
+import clsx from 'clsx';
 import { useEffect, useState, type JSX } from 'react';
 import { Outlet } from 'react-router-dom';
-import { TestId } from 'src/test-utils/constants.ts';
-
-import { Loader } from '@components/Layout/components/Loader/Loader.tsx';
 import styles from './SearchResults.module.scss';
 
 const INITIAL_PAGE = '1';
-const LOADER_VISIBILITY_DURATION = 500;
-const ERROR_ICON_PROPS = {
-  size: 16,
-  color: 'var(--color-accent)',
-};
 
 type SearchResultsProps = {
   query: string;
+  version?: string;
 };
 
-export const SearchResults = ({ query }: SearchResultsProps): JSX.Element => {
+export const SearchResults = ({ query, version }: SearchResultsProps): JSX.Element => {
   const [results, setResults] = useState<CharacterInfo[]>([]);
   const [error, setError] = useState<Error>();
   const [loading, setLoading] = useState(false);
-  const { setParam, getParam, deleteParam } = useCustomSearchParams();
+  const { setParams, getParams, deleteParams } = useCustomSearchParams();
 
-  const page = getParam('page');
-  const detailsId = getParam('details');
+  const [page] = getParams('page');
+  const [detailsId] = getParams('details');
 
   useEffect(() => {
     if (!isNumericInteger(page)) {
-      setParam('page', INITIAL_PAGE);
+      setParams({ page: INITIAL_PAGE });
     }
     if (!isNumericInteger(detailsId)) {
-      deleteParam('details');
+      deleteParams('details');
     }
-  }, [setParam, page, deleteParam, detailsId]);
+  }, [setParams, page, deleteParams, detailsId]);
 
   useEffect(() => {
     setLoading(true);
 
     void getCharactersByName(query, page)
-      .then(infos => {
-        setResults(infos);
-      })
+      .then(setResults)
       .catch((error: unknown) => {
         setError(getErrorInstance(error, ERR_SOMETHING_WRONG));
         setResults([]);
@@ -56,27 +50,25 @@ export const SearchResults = ({ query }: SearchResultsProps): JSX.Element => {
           setLoading(false);
         }, LOADER_VISIBILITY_DURATION);
       });
-  }, [query, page]);
+  }, [query, page, version]);
 
   const handleItemClick = (id: number): void => {
-    setParam('details', id.toString());
+    setParams({ details: id.toString() });
   };
 
   if (!results.length) {
-    const errorMessage = getErrorMessage(error, ERR_SOMETHING_WRONG);
-    return (
-      <pre data-testid={TestId.SearchError} className={styles.errorInfo}>
-        <IconCloseCircleOutline data-testid={TestId.SearchErrorIcon} {...ERROR_ICON_PROPS} />
-        <b data-testid={TestId.SearchErrorMessage}>Error: {errorMessage}</b>
-      </pre>
-    );
+    return <ErrorInfo message={getErrorMessage(error, ERR_SOMETHING_WRONG)} />;
   }
   if (loading) {
     return <Loader />;
   }
   return (
     <div className={styles.results}>
-      <CardList infos={results} onItemClick={handleItemClick} />
+      <CardList
+        className={clsx(detailsId && styles.list)}
+        infos={results}
+        onItemClick={handleItemClick}
+      />
       {detailsId && <Outlet />}
     </div>
   );
