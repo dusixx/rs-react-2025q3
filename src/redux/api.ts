2922be2c6c485623d93f2eabff3122c5dcba68dc
dusixx <113414935+dusixx@users.noreply.github.com>
2025-08-10 17:02:20 +0300

    feat: implement redux api queries

---
+++ src/redux/api.ts
@@ -0,0 +1,42 @@
+import { INITIAL_PAGE } from '@common/constants';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { CharacterInfo, SearchResult } from 'src/redux/api.types.ts';
+import { fetchWithDelay } from './api.utils.ts';
+
+const REQUEST_TIMEOUT = 5000;
+export const BASE_URL = 'https://rickandmortyapi.com/api/';
+export const ALL_INFOS = '';
+
+export const Endpoint = {
+  Character: `${BASE_URL}character/`,
+  Episode: `${BASE_URL}episode/`,
+} as const;
+
+type GetCharacterByNameProps = {
+  name?: string;
+  page?: string | number;
+};
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  fetchFn: fetchWithDelay,
+  timeout: REQUEST_TIMEOUT,
+});
+
+export const rickmortyApi = createApi({
+  reducerPath: 'rickmortyApi',
+  baseQuery,
+  refetchOnReconnect: true,
+  endpoints: build => ({
+    getCharactersByName: build.query<SearchResult, GetCharacterByNameProps>({
+      query: ({ name = ALL_INFOS, page = INITIAL_PAGE }) => ({
+        url: Endpoint.Character,
+        params: { name, page },
+      }),
+    }),
+    getCharacterById: build.query<CharacterInfo, number | string>({
+      query: id => `${Endpoint.Character}/${id.toString()}`,
+    }),
+  }),
+});
+export const { useGetCharactersByNameQuery, useGetCharacterByIdQuery } = rickmortyApi;
--- src/redux/charactersSlice.ts
+++ src/redux/charactersSlice.ts
@@ -1,6 +1,6 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-import type { CharacterInfo } from '@services/api/api.types';
+import type { CharacterInfo } from 'src/redux/api.types';
 
 type InfosState = {
   infos: CharacterInfo[];
