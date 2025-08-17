c8e3d31d068b0b76b78ad50cc72cd6b16ae8ec4e
dusixx <113414935+dusixx@users.noreply.github.com>
2025-08-17 15:02:30 +0300

    refactor: update some components

--- src/components/LangSwitcher/LangSwitcher.tsx
+++ src/components/LangSwitcher/LangSwitcher.tsx
@@ -4,7 +4,7 @@ import { AppLocales } from '@/common/constants/index.ts';
 import { usePathname, useRouter } from '@/i18n/navigation.ts';
 import { useLocale } from 'next-intl';
 import { useSearchParams } from 'next/navigation';
-import type { ReactNode } from 'react';
+import { useTransition, type ReactNode } from 'react';
 import styles from './LangSwitcher.module.scss';
 
 const getLocale = (locale: string, upperCase?: boolean): string => {
@@ -17,15 +17,18 @@ export const LangSwitcher = (): ReactNode => {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const [isPending, startTransition] = useTransition();
 
   const toggleLang = (): void => {
-    router.replace(
-      { pathname, query: Object.fromEntries(searchParams.entries()) },
-      { locale: getLocale(locale) },
-    );
+    startTransition(() => {
+      router.replace(
+        { pathname, query: Object.fromEntries(searchParams.entries()) },
+        { locale: getLocale(locale) },
+      );
+    });
   };
   return (
-    <button className={styles.btn} type='button' onClick={toggleLang}>
+    <button className={styles.btn} type='button' onClick={toggleLang} disabled={isPending}>
       {getLocale(locale, true)}
     </button>
   );
