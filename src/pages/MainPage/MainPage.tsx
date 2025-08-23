6d20200bb22ce16a32d70f39507c9b8e7d980a05
dusixx <113414935+dusixx@users.noreply.github.com>
2025-08-23 04:27:51 +0300

    feat: update MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,48 @@
-import type { ReactNode } from 'react';
+import { ControlledForm } from '@/components/Form/ControlledForm.tsx';
+import { UncontrolledForm } from '@/components/Form/UncontrolledForm';
+import { Modal } from '@/components/Modal/Modal.tsx';
+import { useState, type ReactNode } from 'react';
+import styles from './MainPage.module.scss';
+
+const CONTROLLED_BTN_TEXT = 'Controlled';
+const UNCONTROLLED_BTN_TEXT = 'Uncontrolled';
 
 export default function MainPage(): ReactNode {
-  return <div>main page</div>;
+  const [open, setOpen] = useState(true);
+  const [controlled, setControlled] = useState(true);
+
+  const handleClick = (controlled: boolean = false): void => {
+    setControlled(controlled);
+    setOpen(true);
+  };
+  return (
+    <>
+      <div className={styles.group}>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          {UNCONTROLLED_BTN_TEXT}
+        </button>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            handleClick(true);
+          }}
+        >
+          {CONTROLLED_BTN_TEXT}
+        </button>
+      </div>
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        {controlled ? <ControlledForm /> : <UncontrolledForm />}
+      </Modal>
+    </>
+  );
 }
