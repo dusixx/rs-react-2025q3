708282aa223f70acaff177bfaa24686c948ba6dd
dusixx <113414935+dusixx@users.noreply.github.com>
2025-08-23 19:18:46 +0300

    refactor: update MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,47 +1,62 @@
+import { CardList } from '@/components/CardList/CardList.tsx';
 import { ControlledForm } from '@/components/Form/ControlledForm.tsx';
 import { UncontrolledForm } from '@/components/Form/UncontrolledForm';
 import { Modal } from '@/components/Modal/Modal.tsx';
 import { useState, type ReactNode } from 'react';
 import styles from './MainPage.module.scss';
 
-const CONTROLLED_BTN_TEXT = 'Controlled';
-const UNCONTROLLED_BTN_TEXT = 'Uncontrolled';
+const CONTROLLED_TEXT = 'Controlled';
+const UNCONTROLLED_TEXT = 'Uncontrolled';
 
 export default function MainPage(): ReactNode {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const [controlled, setControlled] = useState(true);
 
   const handleClick = (controlled: boolean = false): void => {
     setControlled(controlled);
     setOpen(true);
   };
+  const closeModal = (): void => {
+    setOpen(false);
+  };
   return (
     <>
-      <div className={styles.group}>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            handleClick();
-          }}
-        >
-          {UNCONTROLLED_BTN_TEXT}
-        </button>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            handleClick(true);
-          }}
-        >
-          {CONTROLLED_BTN_TEXT}
-        </button>
+      <div className={styles.btns}>
+        <div className={styles.group}>
+          <button
+            className={styles.btn}
+            onClick={() => {
+              handleClick();
+            }}
+          >
+            {UNCONTROLLED_TEXT}
+          </button>
+          <button
+            className={styles.btn}
+            onClick={() => {
+              handleClick(true);
+            }}
+          >
+            {CONTROLLED_TEXT}
+          </button>
+        </div>
       </div>
+      <CardList />
       <Modal
+        className={styles['modal-content']}
         open={open}
         onClose={() => {
           setOpen(false);
         }}
       >
-        {controlled ? <ControlledForm /> : <UncontrolledForm />}
+        <p className={styles.heading}>
+          ~ <b>{controlled ? CONTROLLED_TEXT : UNCONTROLLED_TEXT}</b> ~
+        </p>
+        {controlled ? (
+          <ControlledForm closeModal={closeModal} />
+        ) : (
+          <UncontrolledForm closeModal={closeModal} />
+        )}
       </Modal>
     </>
   );
