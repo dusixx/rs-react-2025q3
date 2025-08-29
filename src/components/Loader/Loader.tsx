2b8439753d5a1c6df32d300b13c34c8761c694a6
dusixx <113414935+dusixx@users.noreply.github.com>
2025-08-29 23:53:41 +0300

    feat: add Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react-refresh/only-export-components */
+import clsx from 'clsx';
+import type { CSSProperties, JSX } from 'react';
+import styles from './Loader.module.scss';
+
+const DEFAULT_SIZE = 40;
+export const SPINNER_PROPS = {
+  style: { opacity: 0.5 },
+  src: '/spinner.gif',
+  alt: 'spinner',
+  width: DEFAULT_SIZE,
+};
+
+type LoaderProps = {
+  size?: number;
+  className?: string;
+  style?: CSSProperties;
+};
+
+export const Loader = ({ className, style, size = DEFAULT_SIZE }: LoaderProps): JSX.Element => {
+  return (
+    <div className={clsx(styles.loader, className)} style={style}>
+      <img {...SPINNER_PROPS} width={size} height={size} />
+    </div>
+  );
+};
