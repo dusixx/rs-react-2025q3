8a4c7b8cb7029524e3736adfada60de1cbc084ac
dusixx <113414935+dusixx@users.noreply.github.com>
2026-04-24 01:06:03 +0300

    fix: add localStrorageMock

--- src/test-utils/vitest.setup.ts
+++ src/test-utils/vitest.setup.ts
@@ -2,6 +2,18 @@ import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach, vi } from 'vitest';
 
+const store: Record<string, string> = {};
+
+export const localStorageMock = {
+  setItem: vi.fn((key: string, value: string): void => {
+    store[key] = value;
+  }),
+  getItem: vi.fn((key: string): string | undefined => {
+    return store[key];
+  }),
+};
+vi.stubGlobal('localStorage', localStorageMock);
+
 beforeAll(() => {
   vi.stubGlobal('console', {
     log: vi.fn(),
